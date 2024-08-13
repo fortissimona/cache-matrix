ac755f2e0c6efad24326350eb10ffc2a5a9a299b
fortissimona <srb8463@rit.edu>
2024-08-12 22:37:31 -0400

    The original code files

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,15 @@
-setwd("~/R/ProgrammingAssignment2-master")
+## Put comments here that give an overall description of what your
+## functions do
 
-## Setting input x as a matrix
-## Solving value "i" as a NULL
-## Changed references from "mean" to "inverse"
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(solve) i <<- inverse
-  getInverse <- function() i
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+
 }
 
 
-## Computing the inverse of the matrix created by using a function below
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        i <- x$getInverse()
-        if(!is.null(i)) {
-          message("getting inverse matrix")
-          return(i)
-        }
-        data <- x$get()
-        i <- inverse(data)
-        x$setInverse(i)
-        i
+        ## Return a matrix that is the inverse of 'x'
 }
